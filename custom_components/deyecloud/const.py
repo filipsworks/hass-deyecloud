1279d1404776fea1c1845f2a48414a223e319eac
filipsworks <47939098+filipsworks@users.noreply.github.com>
2026-03-28 14:37:54 +0100

    Add work mode constants to const.py

--- custom_components/deyecloud/const.py
+++ custom_components/deyecloud/const.py
@@ -6,3 +6,11 @@ CONF_APP_ID = "app_id"
 CONF_APP_SECRET = "app_secret"
 CONF_BASE_URL = "base_url"
 CONF_START_MONTH = "start_month"
+
+# Define work mode constants
+SELLING_FIRST = "selling_first"
+ZERO_EXPORT_TO_LOAD = "zero_export_to_load"
+ZERO_EXPORT_TO_CT = "zero_export_to_ct"
+GREEN_POWER_MODE = "green_power_mode"
+FULL_CHARGE_MODE = "full_charge_mode"
+CUSTOMIZED_MODE = "customized_mode"
